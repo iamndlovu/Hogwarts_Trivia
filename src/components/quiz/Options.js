7c1b79169ff3f44f0cbfa6275f180be3fb523712
iamndlovu <ndlovu.p@protonmail.com>
2021-02-10 14:43:52 +0200

    component to contain answers

---
+++ src/components/quiz/Options.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Options = () => {
+	return (
+		<section className="Options">
+			<p>Option a</p>
+			<p>Option b</p>
+			<p>Option c</p>
+		</section>
+	);
+};
+
+export default Options;
