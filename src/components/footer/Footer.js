68ffb0501b3ebc7d75ef0510bc57ec8fc76fa399
iamndlovu <ndlovu.p@protonmail.com>
2021-02-10 14:39:42 +0200

    fixed height for footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,10 +4,8 @@ const Footer = ({ appName }) => {
 	return (
 		<footer style={{
 			background: "#dbe2ef",
-			position: "fixed",
-			bottom: 0,
-			left: 0,
 			width: "100%",
+			height: "2rem",
 			textAlign: "center"
 		}}>
 			&copy;{ `${new Date().getFullYear()} ${appName}` } 
