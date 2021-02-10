193d4b103a5358399f55edd2a808ebe2bc22f14d
iamndlovu <ndlovu.p@protonmail.com>
2021-02-10 14:46:41 +0200

    import quiz container component to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Quiz from './components/quiz/Quiz'
 import Footer from './components/footer/Footer'
 import appLogo from "./Hogwarts-Logo.png";
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header appLogo={ appLogo } appName="Hogwarts Trivia" />
-      <div>Quiz</div>
+      <Quiz />
       <Footer appName="Hogwarts Trivia" />
     </div>
   );
