cd9d54617dbc5e808f6a699cd6f90e5b59cb839f
iamndlovu <ndlovu.p@protonmail.com>
2021-02-13 15:14:38 +0200

    style finished component

--- src/components/quiz/FinishedComp.js
+++ src/components/quiz/FinishedComp.js
@@ -5,16 +5,46 @@ const FinishedComp = ({ stats }) => {
 
   return (
     <div>
-      <h1>Congratulations you have completed the quiz!</h1>
-        <section>
-          <h3>Your Stats:</h3>
-          <ul>
-            <li>Questions Attempted: { ` ${totalQuestions}` }</li>
-            <li>Your Score: { ` ${100 * score / totalQuestions}%` }</li>
-            <li>High Score: { ` ${100 * highScore / totalQuestions}%` }</li>
+      <h1 style={{
+        fontSize: "1.3rem",
+        marginBottom: "1.7rem",
+        color: "green"
+      }}>CONGRATULATIONS YOU HAVE COMPLETED THE QUIZ!</h1>
+        <section style={{
+          background: "#008045b3",
+          color: "#f9f7f7",
+          borderRadius: "7px",
+          minHeight: "10rem",
+          padding: "1.5rem 0.5rem",
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "left",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "1.32rem",
+        }}>
+          <h3>Your Stats</h3>
+          <i style={{
+            width: "60%",
+            height: "2px",
+            margin: "9px",
+            background: "#eee"
+          }}></i>
+          <ul style={{ listStyle: "none" }}>
+            <li><span style={{ fontWeight: "500" }}>Questions Attempted</span>: { ` ${totalQuestions}` }</li>
+            <li><span style={{ fontWeight: "500" }}>Your Score</span>: { ` ${100 * score / totalQuestions}%` }</li>
+            <li><span style={{ fontWeight: "500" }}>High Score</span>: { ` ${100 * highScore / totalQuestions}%` }</li>
           </ul>
         </section>
-      <button>RESTART</button>
+      <button style={{
+        padding: "1rem 2rem",
+        margin: "1rem",
+        fontSize: "1.2rem",
+        background: "#3f72af",
+        border: "transparent",
+        borderRadius: "5px",
+        color: "aliceblue"
+      }}>RESTART</button>
     </div>
   )
 }
